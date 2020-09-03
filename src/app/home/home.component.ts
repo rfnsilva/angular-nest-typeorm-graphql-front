f1d5b1dd8e1e170c2d60c78dc66ae53b0f57d807
rfnsilva <rfnsbadoo@gmail.com>
2020-09-03 04:18:01 -0300

    ajuste no front

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,6 +12,12 @@ import gql from 'graphql-tag';
 export class HomeComponent implements OnInit {
   @Input()
   public produtos: any;
+  @Input()
+  public comparar_boolean: boolean = false;
+  @Input()
+  public verifica_qtd_comparar_boolean: boolean = true;
+
+  public qtd_comparar: number = 0;
 
   constructor(private apollo: Apollo) { }
 
@@ -29,6 +35,8 @@ export class HomeComponent implements OnInit {
             valor,
             categoriaId,
             fornecedorId,
+            checked,
+            estoque,
             createdAt
           }
         }
@@ -38,6 +46,25 @@ export class HomeComponent implements OnInit {
         let aux: any = data;
         this.produtos = aux.getProdutos;
       })
+  }
+
+  checkbox(id: number) {
+    for (let i = 0; i < this.produtos.length; i++){
+      if (this.produtos[i].id === id) {
+        this.produtos[i].checked = !(this.produtos[i].checked);
+        if (this.produtos[i].checked === true) {
+          this.qtd_comparar++;
+          console.log(this.qtd_comparar)
+          if (this.qtd_comparar === 3) {
+            this.verifica_qtd_comparar_boolean = !(this.verifica_qtd_comparar_boolean);
+          }
+        } else {
+          console.log(this.qtd_comparar)
+          this.qtd_comparar--;
+        }
+      }
+    }
+    this.comparar_boolean = true;
   }
 
   javascript() {
